0436d2b5d017bc6091da66204df379ccf64f7e76
manish-tsl <57127937+manish-tsl@users.noreply.github.com>
2020-11-16 19:43:07 +0530

    Initial Commit

---
+++ AlineaInvest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlineaInvest
+//
+//  Created by Silverlabs on 16/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
