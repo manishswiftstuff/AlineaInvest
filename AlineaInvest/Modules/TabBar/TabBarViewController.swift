ef76e783e4d92082762b5aff9e1f60a04e24d0bf
manish-tsl <57127937+manish-tsl@users.noreply.github.com>
2020-11-16 20:20:05 +0530

    project setup
    added TabBarViewController, HomeViewController, ExploreViewController
    added pods Anchorage for UI, XLPagerTabStrip for pager on explore
    assets for home and explore tab
    created structure for other modules

---
+++ AlineaInvest/Modules/TabBar/TabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TabBarViewController.swift
+//  AlineaInvest
+//
+//  Created by Manish Kumar on 16/11/20.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    var controllers = [UIViewController]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTabBarItems()
+    }
+}
+
+private extension TabBarViewController {
+    func setUpTabBarItems() {
+        self.tabBar.tintColor = UIColor(red: 77/255, green: 84/255, blue: 200/255, alpha: 1)
+        setHomeTab()
+        setExploreTab()
+        self.viewControllers = controllers
+        self.selectedIndex = 0
+    }
+    
+    func setHomeTab() {
+        let item = HomeViewController()
+        let icon = UITabBarItem(title: "Home", image: UIImage(named: "ic_home"), tag: 0)
+        item.tabBarItem = icon
+        controllers.append(item)
+    }
+    
+    func setExploreTab() {
+        let item = ExploreViewController()
+        let icon = UITabBarItem(title: "Explore", image: UIImage(named: "ic_explore"), tag: 0)
+        item.tabBarItem = icon
+        controllers.append(item)
+    }
+}
+
